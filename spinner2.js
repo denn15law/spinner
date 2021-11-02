20e1e0165e6d05c575705359914cbf237c62f924
Dennis Law <denn15_law@live.ca>
2021-11-02 18:32:47 +0000

    refractor code to use less lines

--- spinner2.js
+++ spinner2.js
@@ -1,11 +1,7 @@
 const animation = ['|', '/', '-', '\\', '-', '/', '|'];
 let duration = 100;
 for (const frame of animation) {
-  setTimeout(() => {
-    process.stdout.write(`\r${frame}   `);
-  }, duration)
+  setTimeout(() => {process.stdout.write(`\r${frame}   `)}, duration)
   duration += 200;
 }
-setTimeout(() => {
-  process.stdout.write('\n');
-}, 1400);
+setTimeout(() => {process.stdout.write('\n')}, duration += 100);

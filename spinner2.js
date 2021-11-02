e7a1ce76c56f2d9de6286adc7e16e7a31404747f
Dennis Law <denn15_law@live.ca>
2021-11-02 18:28:53 +0000

    add spinner2 which refractors spinner1 to remove repetitive code

---
+++ spinner2.js
@@ -0,0 +1,11 @@
+const animation = ['|', '/', '-', '\\', '-', '/', '|'];
+let duration = 100;
+for (const frame of animation) {
+  setTimeout(() => {
+    process.stdout.write(`\r${frame}   `);
+  }, duration)
+  duration += 200;
+}
+setTimeout(() => {
+  process.stdout.write('\n');
+}, 1400);
